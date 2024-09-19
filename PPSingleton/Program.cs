c23df3cc4d1054abc5f7de116081cc4e6639fdbb
chrispimjose <jose.chrispim@animaeducacao.com.br>
2024-09-19 18:24:16 -0300

    Adicionar arquivos de projeto.

---
+++ PPSingleton/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PPSingleton
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Tenta criar duas instâncias diferentes de Singleton.
+			Singleton s1 = Singleton.GetInstance();
+			Singleton s2 = Singleton.GetInstance();
+
+			// Chama um método da instância Singleton.
+			s1.ShowMessage();
+
+			// Verifica se s1 e s2 são a mesma instância (devem ser).
+			if (s1 == s2)
+			{
+				Console.WriteLine("s1 e s2 são a mesma instância.");
+			}
+			else
+			{
+				Console.WriteLine("s1 e s2 são instâncias diferentes.");
+			}
+		}
+	}
+}
---
+++ PPSingleton/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPSingleton
+{
+	public class Singleton
+	{
+		// O campo estático que contém a única instância da classe Singleton
+		private static Singleton _instance;
+
+		// Construtor privado garante que a classe não possa ser instanciada de fora.
+		private Singleton()
+		{
+			Console.WriteLine("Instância Singleton criada.");
+		}
+
+		// Método estático público que controla o acesso à instância Singleton.
+		public static Singleton GetInstance()
+		{
+			// Verifica se a instância já foi criada; se não, cria uma nova.
+			if (_instance == null)
+			{
+				_instance = new Singleton();
+			}
+
+			// Retorna a instância única da classe.
+			return _instance;
+		}
+
+		// Um exemplo de método que pode ser chamado a partir da instância Singleton
+		public void ShowMessage()
+		{
+			Console.WriteLine("Método do Singleton chamado!");
+		}
+	}
+}
